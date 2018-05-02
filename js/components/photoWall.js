0501293e531ac526b34bb6302bf7ba5773c41f4a
liuyahui0126 <liuyahui_0126@126.com>
2018-05-02 22:37:15 +0800

    twice update

--- js/components/photoWall.js
+++ js/components/photoWall.js
@@ -1,39 +1,14 @@
- class ImgFigure extends React.Component{
-    render(){
-        return(
-            <figure>
-                <img src=""/>
-                <figcaption>
-                    <h3>Have a good trip</h3>
-                    <h3></h3>
-                </figcaption>
-            </figure>
-        )
-    }
-}
-class Controller extends React.Component{
-    render(){
-        return(
-            <span></span>
-        )
-    }
-}
-class Photowall extends React.Component{
-    render(){
-    console.log(111);
-        var imgArr = [];
-        imgArr.forEach(function(item,index){
-            imgArr.push(<ImgFigure/>);
-         }) 
-        return(
-            <section>
-                {imgArr}
-            </section>
-        )
-    }
-}
+console.log(123);
+// class Photowall extends React.Component{
+//     render(){
+//     console.log(111);
+//         return(
+//             <h1>111</h1>
+//         )
+//     }
+// }
 
-ReactDOM.render(
-    <Photowall/>,
-    document.getElementById('photowall')
-);
+// ReactDOM.render(
+//     <Photowall/>,
+//     document.getElementById('photowall')
+// );
